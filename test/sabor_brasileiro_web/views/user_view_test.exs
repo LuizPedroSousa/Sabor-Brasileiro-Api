1cd358f4fee451d0d43783e77ed781bcb66b30c9
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-07-18 23:31:39 -0300

    test(views/user_view): add test to render authenticate user view :white_check_mark:

--- test/sabor_brasileiro_web/views/user_view_test.exs
+++ test/sabor_brasileiro_web/views/user_view_test.exs
@@ -6,33 +6,22 @@ defmodule SaborBrasileiroWeb.UserViewTest do
   alias SaborBrasileiroWeb.UserView
   alias SaborBrasileiro.{User, UserAvatar}
 
-  test "renders create_user.json" do
-    params = %{
-      "name" => Person.PtBr.first_name(),
-      "surname" => Person.PtBr.last_name(),
-      "nickname" => Internet.user_name(),
-      "email" => Internet.email(),
-      "password" => "12345678",
-      "avatar" => %{
-        "url" => Avatar.image_url()
-      }
-    }
+  setup do
+    create_user()
+  end
 
-    {:ok,
-     %User{
-       id: user_id,
-       email: email,
-       name: name,
-       surname: surname,
-       nickname: nickname,
-       inserted_at: user_inserted_at,
-       user_avatar: %UserAvatar{
-         id: avatar_id,
-         url: avatar_url,
-         inserted_at: avatar_inserted_at
-       }
-     } = user} = SaborBrasileiro.create_user(params)
-
+  test "renders create_user.json", %{
+    user:
+      %User{
+        id: user_id,
+        name: name,
+        surname: surname,
+        email: email,
+        nickname: nickname,
+        inserted_at: user_inserted_at,
+        user_avatar: %UserAvatar{id: avatar_id, url: avatar_url, inserted_at: avatar_inserted_at}
+      } = user
+  } do
     response = render(UserView, "create_user.json", user: user)
 
     expected_response = %{
@@ -54,5 +43,56 @@ defmodule SaborBrasileiroWeb.UserViewTest do
     }
 
     assert expected_response == response
+  end
+
+  test "renders authenticate_user.json", %{
+    user:
+      %User{
+        id: user_id,
+        name: name,
+        surname: surname,
+        email: email,
+        nickname: nickname,
+        inserted_at: user_inserted_at,
+        user_avatar: %UserAvatar{id: avatar_id, url: avatar_url, inserted_at: avatar_inserted_at}
+      } = user
+  } do
+    response = render(UserView, "authenticate_user.json", user: user)
+
+    expected_response = %{
+      ok: "User authenticated with successfully",
+      user: %{
+        avatar: %{
+          id: avatar_id,
+          inserted_at: avatar_inserted_at,
+          url: avatar_url,
+          user_id: user_id
+        },
+        email: email,
+        id: user_id,
+        inserted_at: user_inserted_at,
+        name: name,
+        nickname: nickname,
+        surname: surname
+      }
+    }
+
+    assert expected_response == response
+  end
+
+  defp create_user do
+    params = %{
+      "name" => Person.PtBr.first_name(),
+      "surname" => Person.PtBr.last_name(),
+      "nickname" => Internet.user_name(),
+      "email" => Internet.email(),
+      "password" => "12345678",
+      "avatar" => %{
+        "url" => Avatar.image_url()
+      }
+    }
+
+    {:ok, %User{} = user} = SaborBrasileiro.create_user(params)
+    %{user: user, params: params}
   end
 end
