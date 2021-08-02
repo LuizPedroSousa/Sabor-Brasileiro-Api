b503ce44c1152670cd867890287664bfd610e334
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-08-02 10:20:26 -0300

    feat(seeds.exs): add more cakes in seed :sparkles:

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,45 +1,18 @@
-<<<<<<< HEAD
-alias SaborBrasileiro.{Repo, User, UserAvatar}
-alias Ecto.Changeset
-
-user_params = %{
-=======
 alias SaborBrasileiro.{Repo, User}
 alias Ecto.Changeset
 
 user_admin_params = %{
->>>>>>> 57749150f91dd12e522122b5c41b9f146d214346
   "name" => "Crispim Borges",
   "surname" => "Lemos",
   "email" => "crispimborges@gmail.com",
   "password" => "123456",
-<<<<<<< HEAD
-  "nickname" => "crispimborges"
-}
-
-user_avatar_params = %{
-=======
   "nickname" => "crispimborges",
->>>>>>> 57749150f91dd12e522122b5c41b9f146d214346
   "avatar" => %{
     "url" =>
       "https://scontent.fcgh37-1.fna.fbcdn.net/v/t1.6435-9/75588105_10212710874201644_8796001797102632960_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=AO5XpWOWO0MAX9mq9k3&_nc_ht=scontent.fcgh37-1.fna&oh=460ecae06da142dc792108ef4ed03740&oe=6122D61F"
   }
 }
 
-<<<<<<< HEAD
-%User{id: user_id} =
-  user_params
-  |> User.changeset()
-  |> Repo.insert!()
-  |> Changeset.change(%{isAdmin: true})
-  |> Repo.update!()
-
-user_avatar_params
-|> Map.merge(%{"user_id" => user_id})
-|> UserAvatar.changeset()
-|> Repo.insert!()
-=======
 # First User admin
 {:ok, %User{} = user_admin} =
   user_admin_params
@@ -102,6 +75,15 @@ cake_categories_params = [
   },
   %{
     "name" => "Bolos com Cobertura"
+  },
+  %{
+    "name" => "Bolos Caseiros"
+  },
+  %{
+    "name" => "Bolos no pote"
+  },
+  %{
+    "name" => "Coberturas"
   }
 ]
 
@@ -412,6 +394,547 @@ cakes_params = [
       }
     ],
     "price" => "40.00"
+  },
+  %{
+    "category" => "Bolos Caseiros",
+    "name" => "Bolo de milho cremoso",
+    "kg" => "900kg",
+    "ingredients" => [
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "Açucar"
+      },
+      %{
+        "name" => "Ovos"
+      },
+      %{
+        "name" => "Milho"
+      },
+      %{
+        "name" => "Milharina"
+      },
+      %{
+        "name" => "óleo"
+      },
+      %{
+        "name" => "coco ralado"
+      },
+      %{
+        "name" => "queijo parmesão"
+      },
+      %{
+        "name" => "fermentos químicos."
+      }
+    ],
+    "price" => "26.00",
+    "description" =>
+      "Contém Lactose. Alérgicos: contém leite e derivados, derivados da soja, ovo e traços de trigo. Não Contém Glúten*.*O bolo leva ingredientes selecionados e é produzido com o objetivo de não conter as indicações acima, porém, por ser produzido em ambientes que possuem, podem conter traços.",
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102191606_twJv_f.png"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos Caseiros",
+    "name" => "Bolo de fubá",
+    "description" =>
+      "Bolo quentinho ótimo para tomar com aquele café. Contém Glúten E Lactose. Alérgicos: contém leite e derivados, derivados do trigo, derivados da soja e ovo",
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102171739_pmCp_f.png"
+      }
+    ],
+    "ingredientes" => [
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "acúcar"
+      },
+      %{
+        "name" => "ovo"
+      },
+      %{
+        "name" => "óleo vegetal"
+      },
+      %{
+        "name" => "fubá"
+      },
+      %{
+        "name" => "farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "queijo parmesão"
+      },
+      %{
+        "name" => "fermentos químicos pirofosfato de sódio"
+      },
+      %{
+        "name" => "bicarbonato de sódio e fosfato monocálcico"
+      }
+    ],
+    "kg" => "900g",
+    "price" => "24.00"
+  },
+  %{
+    "category" => "Bolos Caseiros",
+    "name" => "Bolo de mandioca",
+    "price" => "28.00",
+    "kg" => "1050KG",
+    "ingredients" => [
+      %{
+        "name" => "Mandioca"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "ovo"
+      },
+      %{
+        "name" => "queijo parmesão"
+      },
+      %{
+        "name" => "coco ralado"
+      },
+      %{
+        "name" => "leite integral"
+      },
+      %{
+        "name" => "fermentos quimicos"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102201604_fE6M_f.png"
+      }
+    ],
+    "description" =>
+      "Contém Lactose. Alérgicos: contém leite e derivados, ovo e traços de trigo. Não Contém Glúten*"
+  },
+  %{
+    "name" => "Fofinho Limão com Gotas de Chocolate",
+    "category" => "Bolos Caseiros",
+    "description" =>
+      "Contém Glúten. Zero Lactose. Alérgicos: contém derivados do trigo, derivados da soja e ovo. O bolo leva ingredientes selecionados e é produzido com o objetivo de não conter as indicações acima, porém, por ser produzido em ambientes que possuem, podem conter traços.",
+    "price" => "28.00",
+    "kg" => "1000KG",
+    "ingredients" => [
+      %{
+        "name" => "Suco de limão"
+      },
+      %{
+        "name" => "farinha de trigo"
+      },
+      %{
+        "name" => "ovo"
+      },
+      %{
+        "name" => "açúcar refinado"
+      },
+      %{
+        "name" => "óleo"
+      },
+      %{
+        "name" => "fermentos químicos"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102171844_7IPi_f.png"
+      }
+    ]
+  },
+  %{
+    "name" => "Fuba com Goiabada",
+    "category" => "Bolos Caseiros",
+    "description" =>
+      "Contém Glúten E Lactose. Alérgicos: contém leite e derivados, derivados do trigo, derivados da soja e ovo.",
+    "price" => "26.00",
+    "kg" => "950g",
+    "ingredients" => [
+      %{
+        "name" => "150 grs de goiabada dentro do bolo"
+      },
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "ovo"
+      },
+      %{
+        "name" => "óleo vegetal"
+      },
+      %{
+        "name" => "goiabada"
+      },
+      %{
+        "name" => "fubá"
+      },
+      %{
+        "name" => "farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "queijo parmesão"
+      },
+      %{
+        "name" => "fermentos químicos"
+      },
+      %{
+        "name" => "pirofosfato de sódio"
+      },
+      %{
+        "name" => "bicarbonato de sódio e fosfato monocálcico"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102171748_7Tn9_f.png"
+      }
+    ]
+  },
+  %{
+    "name" => "Bolo Fofinho de Chocolate",
+    "category" => "Bolos Caseiros",
+    "description" =>
+      "Contém Glúten. Alérgicos: contém derivados do leite, derivados do trigo, derivados da soja e ovo.",
+    "price" => "24.00",
+    "kg" => "800g",
+    "ingredients" => [
+      %{
+        "name" => "Farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "ovos"
+      },
+      %{
+        "name" => "óleo vegetal"
+      },
+      %{
+        "name" => "chocolate em pó"
+      },
+      %{
+        "name" => "fermentos químicos"
+      },
+      %{
+        "name" => "bicarbonato de sódio"
+      },
+      %{
+        "name" => "pirofosfato de sódio e fosfato monocálcico"
+      },
+      %{
+        "name" => "amido de milho"
+      },
+      %{
+        "name" => "e sal"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102171843_zA5f_f.png"
+      }
+    ]
+  },
+  %{
+    "name" => "Cenoura com Gotas de Chocolate",
+    "category" => "Bolos Caseiros",
+    "description" =>
+      "Contém Glúten E Lactose. Alérgicos: contém derivados do leite, derivados do trigo, derivados da soja e ovo",
+    "price" => "28.00",
+    "kg" => "800g",
+    "ingredients" => [
+      %{
+        "name" => "Cenouras"
+      },
+      %{
+        "name" => "farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "ovos"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "óleo vegetal"
+      },
+      %{
+        "name" => "gotas de chocolate"
+      },
+      %{
+        "name" => "fermentos químicos"
+      },
+      %{
+        "name" => "pirofosfato de sódio"
+      },
+      %{
+        "name" => "bicarbonato de sódio"
+      },
+      %{
+        "name" => "e fosfato monocálcico"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102171824_Mn6Y_f.png"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos no pote",
+    "name" => "Leite Ninho com Morango (pote)",
+    "description" => "Bolo no pote ótimo para comer como sobremesa.",
+    "price" => "14.00",
+    "kg" => "300g",
+    "ingredients" => [
+      %{
+        "name" => "açucar"
+      },
+      %{
+        "name" => "farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "morangos"
+      },
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "Leite ninho"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102191158_oQPq_f.png"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos no pote",
+    "name" => "Ninho com Nutella (pote)",
+    "description" => "Bolo no pote ótimo para comer como sobremesa.",
+    "price" => "14.00",
+    "kg" => "300g",
+    "ingredients" => [
+      %{
+        "name" => "açucar"
+      },
+      %{
+        "name" => "farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "Nutella"
+      },
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "Leite ninho"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102191643_YclZ_f.png"
+      }
+    ]
+  },
+  %{
+    "category" => "Coberturas",
+    "name" => "Cobertura Goiabada derretida",
+    "description" => "Cobertura feita com: Goiabada derretida",
+    "price" => "14.00",
+    "kg" => "300g",
+    "ingredients" => [
+      %{
+        "name" => "Goiaba"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202101211112_4dGn_.jpeg"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos Recheados",
+    "name" => "Bolo Prestígio com Recheio",
+    "description" =>
+      "Bolo fofinho de chocolate recheado com Prestígio, coberto com ganache de chocolate ao leite e raspas de chocolate branco.",
+    "price" => "55.00",
+    "kg" => "1.150KG",
+    "ingredients" => [
+      %{
+        "name" => "Chocolates"
+      },
+      %{
+        "name" => "farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "ovos"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "óleo vegetal"
+      },
+      %{
+        "name" => "Ganache"
+      },
+      %{
+        "name" => "Raspas de chocolate branco"
+      },
+      %{
+        "name" => "Coco"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202105221801_6BB4_f.png"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos Recheados",
+    "name" => "Bolo vulcão de brigadeiro com morangos",
+    "description" =>
+      "Contém Glúten, E Lactose. Alérgicos: contém derivados do leite, derivados do trigo, derivados da soja e ovo. Cobertura feita com leite condensado, creme de leite, margarina com sal e chocolate 32%. Recheado com morangos no miolo do bolo.",
+    "price" => "60.00",
+    "kg" => "1000KG",
+    "ingredients" => [
+      %{
+        "name" => "Farinha de trigo enriquecida com ferro e ácido fólico"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "ovos"
+      },
+      %{
+        "name" => "óleo vegeta"
+      },
+      %{
+        "name" => "chocolate em pó"
+      },
+      %{
+        "name" => "fermentos químicos bicarbonato de sódio"
+      },
+      %{
+        "name" => "pirofosfato de sódio"
+      },
+      %{
+        "name" => "amido de milho e sal"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102181932_10uE_f.png"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos Recheados",
+    "name" => "Novo - Vulcão Prestígio",
+    "description" =>
+      "Contém Glúten E Lactose. Alérgicos: contém leite e derivados, derivados do trigo, coco e ovo.",
+    "price" => "58,00",
+    "kg" => "1300g",
+    "ingredients" => [
+      %{
+        "name" => "Chocolates"
+      },
+      %{
+        "name" => "recheio de prestigio"
+      },
+      %{
+        "name" => "coberto com ganache"
+      },
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "farinha de trigo"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "ovos"
+      },
+      %{
+        "name" => "chocolate em pó"
+      },
+      %{
+        "name" => "fermento"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202107271017_TOEI_i.jpg"
+      }
+    ]
+  },
+  %{
+    "category" => "Bolos Recheados",
+    "name" => "Vulcão Nutella Bolo Cenoura com Gotas de Chocolate",
+    "description" => "bem fofinho com o miolo recheado com 600 grs de Nutella.",
+    "price" => "80,00",
+    "kg" => "1000KG",
+    "ingredients" => [
+      %{
+        "name" => "Gotas de chocolate"
+      },
+      %{
+        "name" => "Nutella"
+      },
+      %{
+        "name" => "Cenoutras"
+      },
+      %{
+        "name" => "Leite integral"
+      },
+      %{
+        "name" => "farinha de trigo"
+      },
+      %{
+        "name" => "açúcar"
+      },
+      %{
+        "name" => "ovos"
+      },
+      %{
+        "name" => "fermento"
+      }
+    ],
+    "photos" => [
+      %{
+        "url" =>
+          "https://static-images.ifood.com.br/image/upload/t_high/pratos/90d4317b-69e8-4b7e-9f7a-dd4d6deeb6f8/202102181942_CwSv_f.png"
+      }
+    ]
   }
 ]
 
@@ -419,4 +942,3 @@ cakes_params
 |> Enum.map(fn cake_params ->
   SaborBrasileiro.create_cake(cake_params)
 end)
->>>>>>> 57749150f91dd12e522122b5c41b9f146d214346
